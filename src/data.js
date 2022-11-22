8729b4c8db2bf7fc1f63c6ba926938271d59a109
PrasannaAmale11 <amaleprasanna2001@gmail.com>
2022-11-22 14:35:27 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        id: 1,
+        name: 'Kiran Patil',
+        age: 29,
+        image:
+            'https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+    },
+    {
+        id: 2,
+        name: 'Sandesh Ghodki',
+        age: 32,
+        image:
+            'https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+    },
+    {
+        id: 3,
+        name: 'Sarthak Shinde',
+        age: 36,
+        image:
+            'https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+    },
+    {
+        id: 4,
+        name: 'Sameer Amale',
+        age: 34,
+        image:
+            'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+    },
+    {
+        id: 5,
+        name: 'Siddhant Shinde',
+        age: 29,
+        image:
+            'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+    },
+];
